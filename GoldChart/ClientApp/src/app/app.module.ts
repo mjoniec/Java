99ed2212abe9b0e890a30671462d175e7cc196c6
mjoniec <mart1n.joniec@gmail.com>
2019-03-26 21:20:09 +0100

    Revert "add jqx chart"
    This reverts commit 274906b6b8ac3e2b345ef16112e5d3a69a60c9e4.

--- GoldChart/ClientApp/src/app/app.module.ts
+++ GoldChart/ClientApp/src/app/app.module.ts
@@ -1,33 +1,18 @@
-//import { BrowserModule } from '@angular/platform-browser';
-//import { NgModule } from '@angular/core';
-
-//import { AppRoutingModule } from './app-routing.module';
-//import { AppComponent } from './app.component';
-
-//@NgModule({
-//  declarations: [
-//    AppComponent
-//  ],
-//  imports: [
-//    BrowserModule,
-//    AppRoutingModule
-//  ],
-//  providers: [],
-//  bootstrap: [AppComponent]
-//})
-//export class AppModule { }
-
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-//import { ChartModule } from '../../modules/chart.module'
-import { jqxChartComponent } from '../jqwidgets-ver7.1.0/jqwidgets-ts/angular_jqxchart';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 @NgModule({
-  imports: [BrowserModule, /*ChartModule,*/ CommonModule, FormsModule],
-  declarations: [AppComponent, jqxChartComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
