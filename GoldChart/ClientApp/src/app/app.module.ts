274906b6b8ac3e2b345ef16112e5d3a69a60c9e4
mjoniec <mart1n.joniec@gmail.com>
2019-03-26 21:17:39 +0100

    add jqx chart

--- GoldChart/ClientApp/src/app/app.module.ts
+++ GoldChart/ClientApp/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+
+//import { AppRoutingModule } from './app-routing.module';
+//import { AppComponent } from './app.component';
+
+//@NgModule({
+//  declarations: [
+//    AppComponent
+//  ],
+//  imports: [
+//    BrowserModule,
+//    AppRoutingModule
+//  ],
+//  providers: [],
+//  bootstrap: [AppComponent]
+//})
+//export class AppModule { }
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { ChartModule } from '../../modules/chart.module'
+import { jqxChartComponent } from '../jqwidgets-ver7.1.0/jqwidgets-ts/angular_jqxchart';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, /*ChartModule,*/ CommonModule, FormsModule],
+  declarations: [AppComponent, jqxChartComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
