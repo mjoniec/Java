7a7d4d215c30544c5813664793fd7155fccb8563
mjoniec <mart1n.joniec@gmail.com>
2019-03-27 22:41:44 +0100

    cleanup, smaller chart data

--- GoldChart/ClientApp/src/app/app.component.ts
+++ GoldChart/ClientApp/src/app/app.component.ts
@@ -57,7 +57,7 @@ export class AppComponent {
                 padding: { /*left: 0, right: 0,*/top: 0, bottom: 0 },
                 minValue: new Date(2010, 5, 1),
                 backgroundColor: 'white',
-                dataField: 'Close',
+                dataField: 'Open',
                 baseUnit: 'month',
                 gridLines: { visible: false },
                 serieType: 'area',
@@ -79,7 +79,7 @@ export class AppComponent {
                 type: 'line',
                 toolTipFormatFunction: this.toolTipCustomFormatFn,
                 series: [
-                    { dataField: 'Close', displayText: 'Close Price', lineWidth: 1, lineWidthSelected: 1 }
+                    { dataField: 'Open', displayText: 'Open Price', lineWidth: 1, lineWidthSelected: 1 }
                 ]
             }
         ];
@@ -88,55 +88,3 @@ export class AppComponent {
         args.instance.description = args.minValue.getFullYear() + " - " + args.maxValue.getFullYear();
     }
 }
-
-//import { Component } from '@angular/core';
-
-//@Component({
-//  selector: 'app-root',
-//  templateUrl: './app.component.html',
-//  styleUrls: ['./app.component.css']
-//})
-
-
-//export class AppComponent {
-//  source: any =
-//    {
-//      datatype: 'xml',
-//      datafields: [
-//        { name: 'ProductName', type: 'string' },
-//        { name: 'QuantityPerUnit', type: 'int' },
-//        { name: 'UnitPrice', type: 'float' },
-//        { name: 'UnitsInStock', type: 'float' },
-//        { name: 'Discontinued', type: 'bool' }
-//      ],
-//      root: 'Products',
-//      record: 'Product',
-//      id: 'ProductID',
-//      url: '../assets/products.xml'
-//    };
-
-//  dataAdapter: any = new jqx.dataAdapter(this.source);
-
-//  cellsrenderer = (row: number, columnfield: string, value: string | number, defaulthtml: string, columnproperties: any, rowdata: any): string => {
-//    if (value < 20) {
-//      return `<span style='margin: 4px; float:${columnproperties.cellsalign}; color: #ff0000;'>${value}</span>`;
-//    }
-//    else {
-//      return `<span style='margin: 4px; float:${columnproperties.cellsalign}; color: #008000;'>${value}</span>`;
-//    }
-//  };
-
-//  columns: any[] =
-//    [
-//      { text: 'Product Name', columngroup: 'ProductDetails', datafield: 'ProductName', width: 250 },
-//      { text: 'Quantity per Unit', columngroup: 'ProductDetails', datafield: 'QuantityPerUnit', cellsalign: 'right', align: 'right' },
-//      { text: 'Unit Price', columngroup: 'ProductDetails', datafield: 'UnitPrice', align: 'right', cellsalign: 'right', cellsformat: 'c2' },
-//      { text: 'Units In Stock', datafield: 'UnitsInStock', cellsalign: 'right', cellsrenderer: this.cellsrenderer, width: 100 },
-//      { text: 'Discontinued', columntype: 'checkbox', datafield: 'Discontinued', align: 'center' }
-//    ];
-
-//  columngroups: any[] =
-//    [
-//      { text: 'Product Details', align: 'center', name: 'ProductDetails' }
-//    ];
-//}
